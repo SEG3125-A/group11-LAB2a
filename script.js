0ff121369be9eed257dcbf349d795cf3d49f3c2b
Jaewoon1218 <105750746+Jaewoon1218@users.noreply.github.com>
2024-01-24 15:02:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -198,10 +198,10 @@ function restrictListProducts(prods, restriction) {
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
 	totalPrice = 0;
-	for (let i=0; i<chosenProducts.length; i+=1) {
-		
-			totalPrice += chosenProducts[i].price;
-		
+	for (let i=0; i < products.length; i+=1) {
+			if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+			}
 	}
 	return totalPrice;
 }
